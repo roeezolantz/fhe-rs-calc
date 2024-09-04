7a6bdfd27efeec467c4b4a54e929eb1a098cdcf5
Roee Zolantz <zolantz.roee@gmail.com>
2024-09-04 13:54:07 +0300

    fml keys arent fun

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,25 @@ use lazy_static::lazy_static;
 use std::io::{self, Write};
 use std::panic::{self, UnwindSafe};
 
+// Define a struct to hold both keys
+struct Keys {
+    client_key: tfhe::ClientKey,
+    server_key: tfhe::ServerKey,
+}
+
 // Initialize the server key globally
 lazy_static! {
-    static ref SERVER_KEY: tfhe::ServerKey = generate_server_key();
+    static ref KEYS: Keys = generate_keys_struct();
 }
 
 fn init_server_key() {
-    set_server_key(SERVER_KEY.clone());
+    set_server_key(KEYS.server_key.clone());
 }
 
-fn generate_server_key() -> tfhe::ServerKey {
+fn generate_keys_struct() -> Keys {
     let config = ConfigBuilder::default().build();
-    let (_, server_key) = generate_keys(config);
-    server_key
+    let (client_key, server_key) = generate_keys(config);
+    Keys { client_key, server_key }
 }
 
 // Macro to implement the trait for FHE types
@@ -97,7 +103,7 @@ fn measure_and_execute<T, U, F>(
     init_server_key();
 
     // Ensure the server key is set
-    let result = panic::catch_unwind(|| {
+    let result = panic::catch_unwind(std::panic::AssertUnwindSafe(|| {
         println!("Running monitored operation: {}", operation_name);
         let start = Instant::now();
 
@@ -114,7 +120,7 @@ fn measure_and_execute<T, U, F>(
         println!("Result of {} beween {}, {} = {}, Time Taken: {:?}", operation_name, a, b, decrypted_result, duration);
         io::stdout().flush().unwrap();
         return Ok::<(Duration, U), String>((duration, result));
-    });
+    }));
 
     if let Err(err) = result {
         let error_message = format!("Error in operation '{}': {:?}", operation_name, err);
@@ -146,27 +152,25 @@ where
     let operations: Vec<(&str, Box<dyn FnOnce(&Arc<RwLock<U>>, &Arc<RwLock<U>>) -> U + Send + Sync + UnwindSafe>)> = vec![
         ("Sum", Box::new(|a, b| U::plus(a, b))),
         ("Minus", Box::new(|a, b| U::minus(a, b))),
-        // ("Mul", Box::new(|a, b| U::mul(a, b))),
-        // ("Div", Box::new(|a, b| U::div(a, b))),
+        ("Mul", Box::new(|a, b| U::mul(a, b))),
+        ("Div", Box::new(|a, b| U::div(a, b))),
     ];
-
+    
     // Example usage inside parallel execution
     operations.into_par_iter().for_each(|(name, operation)| {
         println!("Executing opeartion: {:?}", name);
-        let result = measure_and_execute::<T, U, _>(Arc::clone(&encrypted_num1), Arc::clone(&encrypted_num2), client_key, operation, name);
+        let result = measure_and_execute::<T, U, _>(Arc::clone(&encrypted_num1), Arc::clone(&encrypted_num2), client_key, operation, name);        
         println!("Result: {:?}", result);
     });
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config = ConfigBuilder::default().build();
-    let (client_key, _) = generate_keys(config);
-
     init_server_key();
+    let client_key = KEYS.client_key.clone();
 
     // Perform operations on various FHE types
     perform_operation::<u8, FheUint8>(20, 10, &client_key);
-    // perform_operation::<u16, FheUint16>(100, 200, &client_key);
+    perform_operation::<u16, FheUint16>(200, 100, &client_key);
     // perform_operation::<u32, FheUint32>(1000, 2000, &client_key);
 
     Ok(())
